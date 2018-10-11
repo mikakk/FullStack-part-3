96ff9564361250a8bc7f8d1ed55c03bf5814b2d9
Mika Kärkkäinen <mika.karkkainen@koivuniemi.com>
2018-10-11 10:29:55 +0300

    3.3

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Start: `npm run watch`
 
-http://localhost:3001/api/persons
+-   http://localhost:3001/info
+-   http://localhost:3001/api/persons
+-   http://localhost:3001/api/persons/1
--- index.js
+++ index.js
@@ -53,6 +53,12 @@ app.get("/api/persons/:id", (req, res) => {
     }
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    person = persons.filter(person => person.id !== id);
+    res.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
